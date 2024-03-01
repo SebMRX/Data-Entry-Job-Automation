ae70aeb9c110bc4963ac6c4b9c4cb57edab1e0ff
İbrahim Efe Mete Zor <112264660+cyroplex@users.noreply.github.com>
2024-03-01 11:27:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,28 +24,11 @@ data_len = len(link_data)
 option = webdriver.ChromeOptions()
 option.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=option)
-driver.get("https://docs.google.com/forms/d/1yaLcYuEjcKJdOp1Kxt3KjkRzwUSb_EsFg4abqaSdZy0/edit")
-time.sleep(2)
-
-form_address = driver.find_element(By.XPATH,
-                                   '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
-form_price = driver.find_element(By.XPATH,
-                                 '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
-form_link = driver.find_element(By.XPATH,
-                                '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
-submit_button = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
 
 counter = 0
 while data_len > 0:
-    data_len -= 1
-    form_address.send_keys(address[counter])
-    form_price.send_keys(price[counter])
-    form_link.send_keys(link[counter])
-    submit_button.click()
-    time.sleep(1)
-    counter += 1
     driver.get("https://docs.google.com/forms/d/1yaLcYuEjcKJdOp1Kxt3KjkRzwUSb_EsFg4abqaSdZy0/edit")
-    time.sleep(1)
+    time.sleep(2)
     form_address = driver.find_element(By.XPATH,
                                        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
     form_price = driver.find_element(By.XPATH,
@@ -53,6 +36,13 @@ while data_len > 0:
     form_link = driver.find_element(By.XPATH,
                                     '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
     submit_button = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
+    data_len -= 1
+    form_address.send_keys(address[counter])
+    form_price.send_keys(price[counter])
+    form_link.send_keys(link[counter])
+    submit_button.click()
+    time.sleep(1)
+    counter += 1
 
 print("Transmission completed.")
 driver.close()
